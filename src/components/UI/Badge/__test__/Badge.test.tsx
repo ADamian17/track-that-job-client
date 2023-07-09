7241c626884ca1d991c93aa73747f13fd65676e5
Adonis D Martin <adonisdesuvikdo@gmail.com>
2023-07-08 22:21:54 -0700

    added badge test

--- src/components/UI/Badge/__test__/Badge.test.tsx
+++ src/components/UI/Badge/__test__/Badge.test.tsx
@@ -1,1 +1,18 @@
-it("should be in the document", () => { })
+import React from 'react';
+import {
+  RenderResult,
+  render,
+} from "@testing-library/react";
+
+import Badge from ".."
+describe("Badge Component", () => {
+  let tree: RenderResult<typeof import("@testing-library/dom/types/queries"), HTMLElement, HTMLElement>;
+
+  beforeEach(() => {
+    tree = render(<Badge badgeText={''} badgeLink={''} handleClick={() => { }} />)
+  })
+
+  it("should match snapshot", () => {
+    expect(tree).toMatchSnapshot()
+  })
+})
